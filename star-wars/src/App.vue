f1f28911faa0dc424308b3f53648246f2d2608ef
Yarik_Killer-Pico <sologubaroslav504@gmail.com>
2025-08-16 18:37:30 +0300

    add HistorySW

---
+++ star-wars/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <HistoryStarWarsComponent/>
+</div>
+</template>
+
+<script>
+import HistoryStarWarsComponent from './components/HistoryStarWarsComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HistoryStarWarsComponent
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
